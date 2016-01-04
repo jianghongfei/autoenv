dffc83a69c5b78d154f354088b3a75356a026a06
Jiang Hongfei <jiang.hongfei@qq.com>
2016-01-04 12:16:25 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+NOTE
+=======
+
+Cloned from [horosgrisa/autoenv](https://github.com/horosgrisa/autoenv), just make it compatible to bash.
+
+Autoenv
+=======
+
+#### Autoenv automatically sources (known/whitelisted) `.env` and `.out` files.
+
+This plugin support for enter and leave events. By default `.env` is used for entering, and `.out` for leaving. And you can set variable `COLORS=true` for enabling colored output.
+
+## Example of use
+
+- If you are in the directory `/home/user/dir1` and execute `cd /var/www/myproject` this plugin will source following files if they exist 
+```
+/home/user/dir1/.out
+/home/user/.out
+/home/.out
+/var/.env
+/var/www/.env
+/var/www/myproject/.env
+```
+
+- If you are in the directory `/` and execute `cd /home/user/dir1` this plugin will source following files if they exist 
+```
+/home/.env
+/home/user/.env
+/home/user/dir1/.env
+```
+
+- If you are in the directory `/home/user/dir1` and execute `cd /` this plugin will source following files if they exist 
+```
+/home/user/dir1/.out
+/home/user/.out
+/home/.out
+```
+
+## Example of `.env` and `.out` files useful for node.js developing
+
+### .env
+```sh
+nvm use node
+OLDPATH=$PATH
+export PATH=`pwd`/node_modules/.bin:$PATH
+
+```
+
+### .out
+```sh
+nvm use system
+export PATH=$OLDPATH
+
+```
+
---
+++ autoenv.sh
@@ -0,0 +1,100 @@
+if [[ -z $AUTOENV_AUTH_FILE ]]; then
+    AUTOENV_AUTH_FILE=~/.autoenv_authorized
+fi
+
+if [[ -z $COLORS ]]; then
+    COLORS=true
+fi
+
+if [ -n "$ZSH_VERSION" ]; then
+   # assume Zsh
+   chpwd_functions+=( autoenv_init )
+elif [ -n "$BASH_VERSION" ]; then
+   # assume Bash
+   cd() { builtin cd "$@" && autoenv_init; }
+fi
+
+autoenv_init(){
+    _AUTOENV_OLDPATH="$OLDPWD"
+    _AUTOENV_NEWPATH="$(pwd)"
+
+    while [[ ! "$_AUTOENV_NEWPATH" == "$_AUTOENV_OLDPATH"* ]]
+    do
+        if [[ -f "$_AUTOENV_OLDPATH/.out" ]]
+        then
+            check_and_exec "$_AUTOENV_OLDPATH/.out"
+        fi
+        _AUTOENV_OLDPATH="$(dirname $_AUTOENV_OLDPATH)"
+    done
+
+    if [[ $_AUTOENV_OLDPATH == '/' ]]; then
+        _AUTOENV_OLDPATH=''
+    fi
+
+    while [[ ! "$_AUTOENV_OLDPATH" == "$_AUTOENV_NEWPATH"  ]]
+    do
+        _AUTOENV_OLDPATH="$_AUTOENV_OLDPATH$(echo -n '/'; echo ${_AUTOENV_NEWPATH#${_AUTOENV_OLDPATH}} | tr \/ "\n" | sed -n '2p' )"
+        if [[ -f "$_AUTOENV_OLDPATH/.env" ]]
+        then
+            check_and_exec "$_AUTOENV_OLDPATH/.env"
+        fi
+    done  
+}
+
+check_and_run(){
+    if [[ $COLORS == true ]]
+    then
+        echo -e "\e[32m> \e[31mWARNING\e[0m"
+        echo -e "\e[32m> \e[34mThis is the first time you are about to source \e[33m\"\e[31m$1\e[33m\"\e[0m"
+        echo
+        echo -e "\e[32m--------------------------------------------------------------------------------\e[0m"
+        if hash pygmentize 2>/dev/null
+        then
+            echo
+            `whence pygmentize` -f 256 -l shell -g "$1"
+        else
+            echo -e "\e[32m"
+            cat $1
+        fi
+        echo
+        echo -e "\e[32m--------------------------------------------------------------------------------\e[0m"
+        echo
+        echo -ne "\e[34mAre you sure you want to allow this? \e[36m(\e[32my\e[36m/\e[31mN\e[36m) \e[0m"
+    else
+        echo "> WARNING"
+        echo "> This is the first time you are about to source \"$1\""
+        echo
+        echo "--------------------------------------------------------------------------------"
+        echo
+        cat $1
+        echo
+        echo "--------------------------------------------------------------------------------"
+        echo
+        echo -n "Are you sure you want to allow this? (y/N)"
+    fi
+    read answer
+    if [[ "$answer" == "y" ]] || [[ "$answer" == "Y" ]]
+    then
+        echo "$1:$2" >> $AUTOENV_AUTH_FILE
+        envfile=$1
+        shift
+        source $envfile
+    fi
+}
+
+check_and_exec(){
+    if which shasum &> /dev/null
+    then
+        hash=$(shasum "$1" | cut -d' ' -f 1)
+    else
+        hash=$(sha1sum "$1" | cut -d' ' -f 1)
+    fi
+    if grep --quiet "$1:$hash" "$AUTOENV_AUTH_FILE"
+    then
+        envfile=$1
+        shift
+        source $envfile
+    else
+        check_and_run $1 $hash
+    fi
+}
